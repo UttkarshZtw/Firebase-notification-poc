48ad31eff7fc93d1305b679b1d5c13725ab69eb0
UttkarshZtw <121866885+UttkarshZtw@users.noreply.github.com>
2024-04-10 12:50:19 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component, ContentChild, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { environment } from "../environments/environment";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'notification-poc';
+  message:any = null;
+  constructor() {}
+  @ViewChild('optionTemp')
+  optionTeamplate!: TemplateRef<any>;
+  ngOnInit(): void {
+    this.requestPermission();
+    this.listen();
+  }
+  ngAfterViewInit(): void {
+    console.log(this.optionTeamplate);
+  }
+  requestPermission() {
+    const messaging = getMessaging();
+    getToken(messaging, 
+     { vapidKey: environment.firebase.vapidKey}).then(
+       (currentToken) => {
+         if (currentToken) {
+           console.log("Hurraaa!!! we got the token.....");
+           console.log(currentToken);
+         } else {
+           console.log('No registration token available. Request permission to generate one.');
+         }
+     }).catch((err) => {
+        console.log('An error occurred while retrieving token. ', err);
+    });
+  }
+  listen() {
+    const messaging = getMessaging();
+    console.log("messanging -->",messaging)
+    onMessage(messaging, (payload) => {
+      console.log('Message received. ', payload);
+      this.message=payload;
+    });
+  }
 }
